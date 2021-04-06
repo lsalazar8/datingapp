3f6afa175e09c1f8376e05eaf81ac6b74944ba4b
Leidy Salazar <lsalaza8@correo.tdea.edu.co>
2021-04-06 09:01:53 -0500

    Commit inicial

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult <IEnumerable<AppUser>> GetUser()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+    }
+}
